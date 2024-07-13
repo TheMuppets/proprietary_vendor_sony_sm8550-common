95d6133a425e25c9cb46ef8d10c933c3156b682f
Luofan Chen <clfbbn@gmail.com>
2024-07-13 16:29:53 +0000

    fixup! sm8550-common: Declare EGL libs as symlinks during extraction
    Change-Id: I45aa58d3185cf1a2072fb7607777723266e81063

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -1644,5 +1644,5 @@ PRODUCT_PACKAGES += \
     libGLESv2_adreno_libGLESv2_adreno_symlink32 \
     libq3dtools_adreno_libq3dtools_adreno_symlink32 \
     libEGL_adreno_libEGL_adreno_symlink64 \
-    libGLESv2_adreno_libq3dtools_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
     libq3dtools_adreno_libq3dtools_adreno_symlink64
